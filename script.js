f52515951538bfd99efddcf91f6215b7610bca47
mintyry <gitmintyry@gmail.com>
2023-09-02 18:37:39 -0700

    finished code; moved master and pw variables into function so they aren't called upon on load, added += to for loop to continuously add characters, added return statements

--- script.js
+++ script.js
@@ -2,8 +2,8 @@
 var generateBtn = document.querySelector("#generate");
 
 // Array of special characters to be included in password
-let special = ['@', '%', '+', '\\', '/', "'", '!', '#', '$', '^', '?', ':', ',', ')', '(', '}','{', ']', '[', '~', 
-'-', '_', '.'];
+let special = ['@', '%', '+', '\\', '/', "'", '!', '#', '$', '^', '?', ':', ',', ')', '(', '}', '{', ']', '[', '~',
+    '-', '_', '.'];
 console.log(special);
 
 // Array of numeric characters to be included in password
@@ -11,58 +11,92 @@ let num = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 console.log(num);
 
 // Array of lowercase characters to be included in password
-let lower = ['a', 'b', 'c','d', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 
-'u', 'v', 'w', 'x', 'y', 'z'];
+let lower = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
+    'u', 'v', 'w', 'x', 'y', 'z'];
 console.log(lower);
 
 // Array of uppercase characters to be included in password
 let upper = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T',
-'U', 'V', 'W', 'X', 'Y', 'Z'];
+    'U', 'V', 'W', 'X', 'Y', 'Z'];
 console.log(upper);
 
-let master = [];
 
-//prompts for password criteria
-alert('Please follow the subsequent prompts to generate a random password.');
-let userSpecial = confirm('Do you want special characters in your password?');
-let userNum = confirm('Do you want numbers in your password?');
-let userLower = confirm('Do you want lowercase letters in your password?');
-let userUpper = confirm('Do you want uppercase letters in your password?');
-let passwordLength = prompt('How long do you want your password to be?\nPlease enter a value of at least 8, but no more than 128.')
+function generatePassword() {
 
+    let master = [];
 
-function generatePassword () {
+    let pw = '';
+
+    //prompts for password criteria
+    alert('Please follow the subsequent prompts to generate a random password.');
+    let userSpecial = confirm('Do you want special characters in your password?');
+    let userNum = confirm('Do you want numbers in your password?');
+    let userLower = confirm('Do you want lowercase letters in your password?');
+    let userUpper = confirm('Do you want uppercase letters in your password?');
+    let passwordLength = prompt('How long do you want your password to be?\nPlease enter a value of at least 8, but no more than 128.')
+
+    console.log(userSpecial);
+    console.log(userNum);
+    console.log(userLower);
+    console.log(userUpper);
+    console.log(passwordLength);
+
     if (userSpecial === true) {
-        master.concat.special;
+        master = master.concat(special);
     }
     if (userNum === true) {
-        master.concat.num;
+        master = master.concat(num);
     }
     if (userLower === true) {
-        master.concat.lower;
+        master = master.concat(lower);
     }
     if (userUpper === true) {
-        master.concat.upper;
-    };
+        master = master.concat(upper);
+    }
+    /*This is if user doesn't choose any criteria */
+    if (userSpecial === false && userNum === false && userLower === false && userUpper === false) {
+        alert('Please select at least one criteria.');
+        return generatePassword();
+    }
+    /*This is if user enters a number not within our parameters of 8-128*/
+    if (passwordLength < 8 || passwordLength > 128) {
+        alert('Please enter an amount between 8 and 128.');
+        return generatePassword();
+    }
+    console.log(master);
+    /* The following for loop is saying: "For when I start with 0 characters, i will keep adding one more character until
+    I hit how many characters the user indicates they want" */
+    /* When i this entire condition is true, i want to randomize the characters that have been chosen:
+    Math.random be any number between 0 and 1 multiplied by the current master array's length of indices, this way you count
+    only until the end of the master array. Math.Floor then rounds that down to nearest whole number (which would be character's index.)
+    So for example, let's say master.length is 20 characters. With random and floor, characters will be chosen from indices 0-19. 
+    That's what we would like it to do, but as it is written, it only produces a number. 
+    Perhaps master then wrapping entire formula in brackets will treat it as an accessible character*/
 
-    //if i have 0 apples, and i get one more apple, add it to basket, but stop when i have 5
-    // for (let i = 0; i <  ; i++)
-    //  {
-   // master
-    // }
+    for (i = 0; i < passwordLength; i++) {
+        pw += master[Math.floor(Math.random() * master.length)];
+        console.log(pw);
+    }
 
-    return;
+    console.log(pw);
+    return pw;
 }
 
-generatePassword();
+/* so this should determine the password, but where to store it?
+variable (pw) */
+
+/*The return call is used to pass information from one block to another or simply stop the code from continuing */
+
+
+
 
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+    var password = generatePassword();
+    var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+    passwordText.value = password;
 
 }
 
